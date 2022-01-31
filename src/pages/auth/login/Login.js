90d5dc14b54cb8170962f01787ea95284a60097e
Muthukumaran <gmkumaran87@gmail.com>
2022-01-31 15:09:38 +0530

    Modified the Formatter to React

--- src/pages/auth/login/Login.js
+++ src/pages/auth/login/Login.js
@@ -8,80 +8,75 @@ import { useNavigate } from "react-router-dom";
 import "../Form.css";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const { isLoggedIn } = useSelector((state) => state.auth);
-    const { message, status } = useSelector((state) => state.ui);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  const { message, status } = useSelector((state) => state.ui);
 
-    console.log("IN LOGIN component");
-    // console.log("Iniital value", isLoggedIn);
-    useEffect(() => {
-        // Clearing the message and status while starting of the app
-        dispatch(uiActions.clearNotification());
-        if (isLoggedIn) {
-            navigate("/");
-        }
-    }, [isLoggedIn, navigate, dispatch]);
+  console.log("IN LOGIN component");
+  // console.log("Iniital value", isLoggedIn);
+  useEffect(() => {
+    // Clearing the message and status while starting of the app
+    dispatch(uiActions.clearNotification());
+    if (isLoggedIn) {
+      navigate("/");
+    }
+  }, [isLoggedIn, navigate, dispatch]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        dispatch(loginUser({ email, password }));
-        // dispatch(authActions.login({ isLoggedIn: true, isNavBar: false }));
-    };
-    return ( <
-        div className = "login form-center" > {
-            message && ( <
-                div className = {
-                    status === "success" ?
-                    "form-msg alert-success" :
-                        "form-msg alert-danger"
-                } >
-                { message } <
-                /div>
-            )
-        }
-
-        <
-        h2 className = "form-title" > Please Login < /h2> <
-        form className = " form loginForm"
-        onSubmit = { handleSubmit } >
-        <
-        div className = "form-control" >
-        <
-        label htmlFor = "" > Email < /label> <
-        input required type = "text"
-        name = "email"
-        value = { email }
-        onChange = {
-            (e) => setEmail(e.target.value) }
-        /> <
-        /div> <
-        div className = "form-control" >
-        <
-        label htmlFor = "password" > Password < /label> <
-        input required type = "password"
-        name = "password"
-        value = { password }
-        onChange = {
-            (e) => setPassword(e.target.value) }
-        /> <
-        /div> <
-        button className = "btn" > Submit < /button> <
-        span className = "password-link" >
-        <
-        NavLink className = "forgot-link"
-        to = "/forgot-password" >
-        Forgot Password ?
-        <
-        /NavLink> <
-        /span> <
-        /form> <
-        /div>
-    );
+    dispatch(loginUser({ email, password }));
+    // dispatch(authActions.login({ isLoggedIn: true, isNavBar: false }));
+  };
+  return (
+    <div className="login form-center">
+      {" "}
+      {message && (
+        <div
+          className={
+            status === "success"
+              ? "form-msg alert-success"
+              : "form-msg alert-danger"
+          }
+        >
+          {message}{" "}
+        </div>
+      )}
+      <h2 className="form-title"> Please Login </h2>{" "}
+      <form className=" form loginForm" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor=""> Email </label>{" "}
+          <input
+            required
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />{" "}
+        </div>{" "}
+        <div className="form-control">
+          <label htmlFor="password"> Password </label>{" "}
+          <input
+            required
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />{" "}
+        </div>{" "}
+        <button className="btn"> Submit </button>{" "}
+        <span className="password-link">
+          <NavLink className="forgot-link" to="/forgot-password">
+            Forgot Password ?
+          </NavLink>{" "}
+        </span>{" "}
+      </form>{" "}
+    </div>
+  );
 };
 
 export default Login;
